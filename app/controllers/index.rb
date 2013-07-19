5ef238d1059ef9e969ec2369c5c05e41f18b8bc3
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-19 15:30:39 -0700

    users can create account, log in and out

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,20 +1,42 @@
 get '/' do
-  # Look in app/views/index.erb
+  if session[:user_id]
+    @user = User.find(session[:user_id])
+  end
   erb :index
 end
 
+get '/user/signup' do
+  erb :signup
+end
+
+get '/user/login' do
+  erb :login
+end
+
 post '/user/new' do
-
+  username = params[:username]
+  password = params[:password]
+  User.create({username: username, password: password})
+  redirect to('/')
 end
 
-post '/login' do
-
+post '/user/login' do
+  username = params[:username]
+  password = params[:password]
+  user = User.where(username: username).first
+  if user.password == password
+    session[:user_id] = user.id
+  else
+    "Incorrect login"
+  end
+  redirect to('/')
 end
 
-get '/logout'
+get '/logout' do
+  session.clear
+  redirect to('/')
+end
 
-do
-
-get '/round/new'
+get '/round/new' do
 
 end
